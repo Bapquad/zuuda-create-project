7daf983f1435c3de6d87c44588c5b8b9380adaeb
bapquad <9875274+Bapquad@users.noreply.github.com>
2026-02-06 23:47:31 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,8 +1,11 @@
 <?php
-
+// Let's cooment out this code if you have a index.php
 use Zuuda\Route;
+
+$router = array();
 
 // Let's cooment out this code if you have a index.php
 route::get('/', function($res, $req) {
 	echo "Hello World";
 });
+
